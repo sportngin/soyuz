74d840245a8d6dd69f308d1df71c6400a0154e0f
Andy Fleener <andrew.fleener@sportngin.com>
2014-02-18 09:08:53 -0600

    Add basic config valiation that just ensures the file exists and that it results in yaml

---
+++ lib/soyuz/config.rb
@@ -0,0 +1,31 @@
+require 'safe_yaml'
+module Soyuz
+  class Config
+    def initialize(config_file)
+      @config_file = config_file
+    end
+
+    def check
+      if valid?
+        puts "Config file is valid. We are go for launch."
+      else
+        raise InvalidConfig, "Your config file is invalid"
+      end
+    end
+
+  private
+    def config_data
+      return @config_data if @config_data
+      load_config
+    end
+
+    def load_config
+      @config_data = SafeYAML.load_file(@config_file)
+    end
+
+    def valid?
+      File.exists?(@config_file) && config_data.is_a?(Hash)
+    end
+  end
+  InvalidConfig = Class.new(StandardError)
+end
