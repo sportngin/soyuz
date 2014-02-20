e87642c845a98def749052220f9722bd5d5f61ab
Andy Fleener <andrew.fleener@sportngin.com>
2014-02-20 09:27:41 -0600

    add more setup related things to the config object

--- lib/soyuz/config.rb
+++ lib/soyuz/config.rb
@@ -1,4 +1,5 @@
 require 'safe_yaml'
+require_relative 'environment'
 module Soyuz
   class Config
     def initialize(config_file)
@@ -6,20 +7,31 @@ module Soyuz
     end
 
     def check
-      if valid?
-        puts "Config file is valid. We are go for launch."
-      else
-        raise InvalidConfig, "Your config file is invalid"
-      end
+      validate!
+      puts "Config file is valid. We are go for launch."
+    end
+
+    def validate!
+      raise InvalidConfig, "Your config file is invalid" unless valid?
+    end
+
+    def environments
+      @environments ||= config_data[:environments].map{|attributes| Environment.new(attributes, defaults) } if config_data[:environments]
+      @environments ||= []
     end
 
   private
+
+    def defaults
+      config_data[:defaults] || {}
+    end
+
     def config_data
       @config_data ||= load_config
     end
 
     def load_config
-      @config_data = SafeYAML.load_file(@config_file)
+      symbolize_keys(SafeYAML.load_file(@config_file))
     end
 
     def valid?
@@ -28,6 +40,23 @@ module Soyuz
       rescue StandardError
         false
       end
+    end
+
+    def symbolize_keys(hash)
+      return hash unless hash.is_a?(Hash)
+      hash.inject({}){|result, (key, value)|
+        new_key = case key
+                  when String then key.to_sym
+                  else key
+                  end
+        new_value = case value
+                    when Hash then symbolize_keys(value)
+                    when Array then value.map{|el| symbolize_keys(el) }
+                    else value
+                    end
+        result[new_key] = new_value
+        result
+      }
     end
   end
   InvalidConfig = Class.new(StandardError)
