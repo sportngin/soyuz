185e816c98123d1399a361d6982bfcf83fc248f3
Andy Fleener <andrew.fleener@sportngin.com>
2014-02-20 08:47:38 -0600

    Add the command object

---
+++ lib/soyuz/command.rb
@@ -0,0 +1,17 @@
+require_relative 'command_choice'
+module Soyuz
+  class Command
+    def initialize(cmd)
+      raise ArgumentError, "Command must be a string" unless cmd.is_a?(String)
+      @cmd = cmd
+    end
+
+    def self.build(cmd)
+     cmd.is_a?(Array) ? CommandChoice.new(cmd) : new(cmd)
+    end
+
+    def run
+      system(@cmd)
+    end
+  end
+end
